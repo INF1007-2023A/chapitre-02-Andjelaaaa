bdf3c76ff956fe060453166d69e11360602cbde2
kegaub <kegaub@users.noreply.github.com>
2021-09-07 17:39:59 -0400

    Update _exercice_version_prof.py

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
+    nouveau_mot = ""
+    for lettre in mot:
+        nouveau_mot += chr(ord(lettre) - 32)
 
-    return mot
+    return nouveau_mot
 
 
 if __name__ == '__main__':
